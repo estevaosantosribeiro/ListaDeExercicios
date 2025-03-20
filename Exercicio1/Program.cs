9e29abeb8713a8c720a158bc90e98a0f2bd8d608
estevao-santos-ribeiro <estevaosantos@uniplaclages.edu.br>
2025-03-20 15:17:32 -0300

    Implementado exercício 1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal altura, largura, comprimento, volume;
+
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("Calculadora de volume de caixa retangular");
+            Console.WriteLine("---------------------------------------------");
+
+            Console.Write("Digite a altura da caixa: ");
+            altura = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Digite a largura da caixa: ");
+            largura = Convert.ToDecimal(Console.ReadLine());
+            Console.Write("Digite a comprimento da caixa: ");
+            comprimento = Convert.ToDecimal(Console.ReadLine());
+
+            volume = altura * largura * comprimento;
+
+            Console.WriteLine($"O volume da caixa retangular é {volume} cm³");
+
+            Console.ReadLine();
+        }
+    }
+}
